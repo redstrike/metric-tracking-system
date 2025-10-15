7504301d175fc91bb472b4bfbc33c81414c90172
Tung Nguyen <redstrike91dn@gmail.com>
2025-10-15 22:35:28 +0700

    feat(node/bun/elysia): :memo: implement API Reference by using Elysia's OpenAPI plugin

--- node/bun/elysia/src/main.ts
+++ node/bun/elysia/src/main.ts
@@ -1,7 +1,16 @@
+import { fromTypes, openapi } from '@elysiajs/openapi'
 import { Elysia } from 'elysia'
 import { mapAllEndpoints } from './routes'
 
-const app = new Elysia()
+export const app = new Elysia()
+
+// Reference: https://elysiajs.com/patterns/openapi
+app.use(
+	openapi({
+		references: fromTypes(process.env.NODE_ENV === 'production' ? 'dist/index.d.ts' : 'src/index.ts'),
+		path: '/docs',
+	})
+)
 
 mapAllEndpoints(app)
 
